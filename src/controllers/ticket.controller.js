dc156228d3f76673e703bda7e6a940c6d1d4a2dd
Fernando Lara <larafernandoagustin@gmail.com>
2023-09-18 17:48:05 -0300

    Added ticket functionality to complete the purchase process.

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -4,7 +4,7 @@ export const generateTicket = async(req, res, next) => {
     try {
         const _id = req.session.passport?.user;
         const ticket = await service.generateTicket(_id)
-        if(!ticket) res.status(404).json({msg: "Error generating ticket"})
+        if(!ticket) res.status(404).json({msg: "Error at generating ticket. You are not logged-in or your cart is empty."})
         res.status(200).json(ticket)
     } catch (error) {
         next(error.message)
